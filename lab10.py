2db5fb1e9ff3dd60fe6501719c7be236d8736c89
MMrok27 <97949402+MMrok27@users.noreply.github.com>
2022-05-18 08:34:21 +0200

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -32,19 +32,11 @@ from PIL import Image
 # x2 = np.arange(0, 31, 0.1)
 # y1 = np.sin(2 * np.pi * x1)
 # y2 = np.cos(2 * np.pi * x2)
-# fig, axs = plt.subplots(2)
-# axs[0].plot(x1, y1, '-', label="sin(x)")
-# axs[0].set_title('wykres sin(x)')
-# axs[0].set_xlabel('x')
-# axs[0].set_ylabel('sin(x)')
-# axs[1].plot(x2, y2, 'r-',label="sin(y)")
-# axs[1].set_title('wykres cos(x)')
-# axs[1].set_xlabel('x')
-# axs[1].set_ylabel('cos(x)')
-# lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
-# lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-# fig.legend(lines, labels)
-# plt.subplots_adjust(hspace=0.5)
+# plt.plot(x1, y1, '-')
+# plt.plot(x2, y2, 'r-')
+# plt.title("sin(x), cos(x)")
+# 
+# 
 # plt.show()
 #4
 # df = pd.read_csv('iris.csv',header=0,sep=',',decimal='.')
